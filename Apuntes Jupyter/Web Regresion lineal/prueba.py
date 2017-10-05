e2e83119fde5b17a0c9468ab820ec152c86fe3dd
Will Arevalo <willi11-95@hotmail.com>
2017-10-05 12:40:26 -0500

    Add excercise without source linear regresion in py

--- Apuntes Jupyter/Web Regresion lineal/prueba.py
+++ Apuntes Jupyter/Web Regresion lineal/prueba.py
@@ -1,20 +1,70 @@
+from colorama import init, Fore, Back, Style #Libreria de colores y stilos de print
 from IPython.display import clear_output
-
-while(True):
-    print("Bienvenido a compucasas.")
-    print("""Menu, digite un numero:
+import os
+b_x = []
+b_t = []
+def clear(): #Funcion para limpiar las diferentes pantallas
+    if os.name == "nt": # Windows
+        os.system("cls")
+    elif os.name == "posix": # Linux / Mac
+        os.system("clear")
+    clear_output(wait=True) #Jupyter
+while(True): #MENU
+    print(Fore.CYAN+"\nBienvenido a compucasas.")
+    opcion = int(input("""\nMenu, digite un numero:
     1)Deseo ingresar los datos.
-    2)Deseo predecir el valor de una casa.
-    99)Salir""")
-    opcion = input()
-    clear_output(wait=True)
-    b_x = []
-    b_t = []
-    print(opcion)
-    if opcion == '1':
-        ipm2 = int(input(print("Introduzca la cantidad de m^2:")))
-        b_x.append(ipm2)
-        print(b_x)
-    if opcion == '99':
-        print ("Hasta la vista baby")
+    2)Imprimir los datos.
+    3)Deseo predecir el valor de una casa.
+    99)Salir\n"""))
+    if opcion == 1: #INGRESO DE DATOS
+        while(True):
+            print(Fore.GREEN+"Registro de datos")
+            x = input("Introduzca el numero de m²:\n")
+            if x.isdigit() and x != '0':
+                b_x.append(x)
+                clear()
+                print(Fore.GREEN+"El valor que introduciste es {}m² \n".format(x))
+                break
+            else:
+                clear()
+                print(Fore.RED+"\nEl dato introducido no es un numero o es 0, {}\n".format(x))
+        while(True):
+            print(Fore.GREEN+"Registro de datos")
+            t = input("Introduzca el precio por piso:\n")
+            if t.isdigit() and t != '0':
+                b_t.append(t)
+                clear()
+                print(Fore.GREEN+"El valor que introduciste es {} pesos \n".format(t))
+                break
+            else:
+                clear()
+                print(Fore.RED+"\nEl dato introducido no es un numero o es 0, {}\n".format(t))
+        print ("¡Datos registrados!")
+    elif opcion == 2: #MUESTRA TABLA DE REGISTROS
+        print(Fore.BLUE+"Los datos ingresados son\n")
+        print(Fore.YELLOW+"M²\tPrecio")
+        for i in range(len(b_t)):
+            print(Fore.GREEN+"{}\t{}".format(b_x[i],b_t[i]))
+        input("Enter para continuar\n")
+        clear()
+    elif opcion == 3: #PRONOSTICO
+        while(True):
+            print(Fore.GREEN+'\nPronostico :)')
+            f = input("Pronostica el valor del piso para este numero de m²:\n")
+            if f.isdigit() and f != '0':
+                forecast = f
+                clear()
+                print(Fore.GREEN+"El valor que introduciste es {} \n".format(forecast))
+                input("Enter para continuar\n")
+                clear()
+                break
+            else:
+                clear()
+                print(Fore.RED+"\nEl dato introducido no es un numero o es 0, {}\n".format(f))
+    elif opcion == 99: #SALIR
+        print (Fore.RED+"Hasta la vista baby. ಠ_ಠ")
         break
+    else: #OPCION INCORRECTA
+        clear()
+        print("Opcion incorrecta, digite otra porfavor.")
+        
